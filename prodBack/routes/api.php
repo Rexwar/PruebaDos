e784ae88e01752f0bf54c2655aff42dfeeaab560
Rexwar <reyvaldesm@gmail.com>
2023-11-22 15:38:37 -0300

    commit ru 15:38

--- prodBack/routes/api.php
+++ prodBack/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,8 +26,3 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-// en routes/api.php
-Route::get('/products', 'ProductController@index');
-Route::post('/products', 'ProductController@store');
-Route::put('/products/{id}', 'ProductController@update');
-Route::delete('/products/{id}', 'ProductController@destroy');
